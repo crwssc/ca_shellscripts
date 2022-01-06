656bd018dbed1b424f9486cdbeb3a2c05edf1444
croissance <myadressemailisthat@gmail.com>
2022-01-06 12:34:52 +0100

    StringOperations

--- BasicStringOperations.sh
+++ BasicStringOperations.sh
@@ -2,13 +2,12 @@
 
 BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
 # write your code here
-ISAY="Life is like a snowball. The important t
-hing is finding wet snow and a really long hill."
+ISAY="Life is like a snowball. The important thing is finding wet snow and a really long hill."
 
 Change1=${ISAY[@]/snow/foot}
 Change2=${Change1[@]// snow/}
 Change3=${Change2[@]//finding/getting}
-Change4=${Change3[@]// ${Change3:56}/}
+Change4=${Change3%w*}
 
 ISAY=$Change4
 

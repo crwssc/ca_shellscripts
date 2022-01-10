0fe3bd6b745382c786031b503592f71a53a7b7eb
croissance <myadressemailisthat@gmail.com>
2022-01-10 10:28:33 +0100

    Correction

--- ArrayComparaison.sh
+++ ArrayComparaison.sh
@@ -4,14 +4,27 @@ a=(3 5 8 10 6)
 b=(6 5 4 12)
 c=(14 7 5 7)
 
-#Comparons a et b, on veut le ou les éléments commun de ces 2 tableaux
+#Correction
 
-for n in ${a[@]} x=0 ; do
-	if [ "$n" == "${b[0]}" ] ; then
-		echo "$n"
-		x+=1
-	else
-		echo "These three arrays don't have common elements"
-	fi 
-done
+for x in "${a[@]}"; do 
+	  in=false 
+	  for y in "${b[@]}"; do 
+	    if [ $x = $y ];then 
+	      # assigning the matching results to new array z
+	      z[${#z[@]}]=$x
+	    fi
+	  done 
+	done
+	#comparison of third array c with new array z
+	for i in "${c[@]}"; do 
+	  in=false
+	  for k in "${z[@]}"; do
+	    if [ $i = $k ];then
+	      # assigning the matching results to new array j
+	      j[${#j[@]}]=$i
+	    fi
+	  done 
+	done 
+	# print content of array j
+	echo ${j[@]}
 

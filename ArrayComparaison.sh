09eeb1ca60509eefb78f355a901f72e15cd01d9a
croissance <myadressemailisthat@gmail.com>
2022-01-10 10:04:59 +0100

    première partie la logique a l'air de marcher

--- ArrayComparaison.sh
+++ ArrayComparaison.sh
@@ -4,14 +4,14 @@ a=(3 5 8 10 6)
 b=(6 5 4 12)
 c=(14 7 5 7)
 
-#Comparons a et b, on veut l'élément commun de ces 2 tableaux
+#Comparons a et b, on veut le ou les éléments commun de ces 2 tableaux
 
-for n in [a] and for y in [b] ; do
-	if [ $n = ${#b[y]} ] ; then
-		z = $n
+for n in ${a[@]} x=0 ; do
+	if [ "$n" == "${b[0]}" ] ; then
+		echo "$n"
+		x+=1
 	else
 		echo "These three arrays don't have common elements"
 	fi 
 done
 
-echo $z
